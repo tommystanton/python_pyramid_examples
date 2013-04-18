31ce9eb28a0b10417b789703d64c3f12e13466c6
Tommy Stanton <tommystanton@gmail.com>
2013-04-18 18:24:02 -0500

    Change route and import xkcd to get random text

--- test.py
+++ test.py
@@ -2,13 +2,17 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 
+import xkcd
+
 def hello_world(request):
-    return Response('Hello %(name)s!' % request.matchdict)
+    comic = xkcd.getRandomComic()
+    text = comic.getAltText()
+    return Response(text)
 
 if __name__ == '__main__':
     config = Configurator()
-    config.add_route('hello', '/hello/{name}')
-    config.add_view(hello_world, route_name='hello')
+    config.add_route('comic_text', '/comic_text')
+    config.add_view(hello_world, route_name='comic_text')
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
     server.serve_forever()
