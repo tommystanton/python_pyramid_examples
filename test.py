782b1f19ce13a3e3926407cf62dd835eeddb2c9a
Tommy Stanton <tommystanton@gmail.com>
2013-04-18 19:10:22 -0500

    Update function name
    This is no longer a "hello world" web app.

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from pyramid.response import Response
 
 import xkcd
 
-def hello_world(request):
+def get_comic_text(request):
     comic = xkcd.getRandomComic()
     text = comic.getAltText()
     return Response(text)
@@ -12,7 +12,7 @@ def hello_world(request):
 if __name__ == '__main__':
     config = Configurator()
     config.add_route('comic_text', '/comic_text')
-    config.add_view(hello_world, route_name='comic_text')
+    config.add_view(get_comic_text, route_name='comic_text')
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
     server.serve_forever()
